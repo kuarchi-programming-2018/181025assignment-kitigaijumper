c3def475f180a527e4dc75be114f7f355759e56f
kitigaijumper <kazuomoriuti@yahoo.co.jp>
2018-10-26 02:26:27 +0900

    課題

---
+++ recursion-practice/plotrose.py
@@ -0,0 +1,8 @@
+from turtle import *
+from rose import *
+
+#平行四辺形バージョンです。
+reset()
+hideturtle()
+rose_window_recursion([[-200,-200], [100, -200], [200, 200], [-100, 200]], 0.3, 10)
+done()
---
+++ recursion-practice/rose.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 19 17:16:32 2018
+@author: toi1-
+"""
+
+from turtle import *
+
+# 四角い薔薇形を描く関数
+# rose_window_recursion(四角形の4頂点，内分比，繰り返し回数）
+def rose_window_recursion(points, ratio, depth):
+    rectangle(points)
+    new_points = deviding_points(points, ratio)
+    
+    if depth == 0:
+        penup()
+        setpos(-200, -200)
+        
+    else:
+        rose_window_recursion(new_points, ratio, depth - 1)
+
+
+#---------------— 以下は補助的な関数—------------------------
+
+# 内分を実行する関数
+# deviding(点A, 点B, 内分比)
+def deviding(p0, p1, r):
+    return p0 * (1 - r) + p1 * r
+
+
+# 長方形を描く関数
+# rectangle(四角形の4頂点）
+def rectangle(points):
+    [[x0, y0], [x1, y1], [x2, y2], [x3, y3]] = points
+    penup()
+    setpos(x0, y0)
+    pendown()
+    setpos(x1, y1)
+    setpos(x2, y2)
+    setpos(x3, y3)
+    setpos(x0, y0)
+
+
+# 2点の内分点を求める関数
+# deviding_point(点A, 点B, 内分比）
+def deviding_point(p0, p1, ratio):
+    [x0, y0] = p0
+    [x1, y1] = p1
+    xr = deviding(x0, x1, ratio)
+    yr = deviding(y0, y1, ratio)
+    return [xr, yr]
+
+
+# 四角形の各辺の内分点を求める関数
+# deviding_points(四角形の4頂点内分比）
+def deviding_points(points, ratio):
+    [p0, p1, p2, p3] = points
+    pr0 = deviding_point(p0, p1, ratio)
+    pr1 = deviding_point(p1, p2, ratio)
+    pr2 = deviding_point(p2, p3, ratio)
+    pr3 = deviding_point(p3, p0, ratio)
+    return [pr0, pr1, pr2, pr3]# -*- coding: utf-8 -*-
+"""
+Spyderエディタ
+
+これは一時的なスクリプトファイルです
+"""
+
